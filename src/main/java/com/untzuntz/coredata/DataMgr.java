756c679f133815e1d28c7d3caa42990186a7a845
Ariel Himmelstern <ariel@textrecruit.com>
2017-05-26 15:56:33 -0700

    moving to latest mongo driver

--- src/main/java/com/untzuntz/coredata/DataMgr.java
+++ src/main/java/com/untzuntz/coredata/DataMgr.java
@@ -179,7 +179,7 @@ public class DataMgr {
 		DBTableMap tbl = clazz.getAnnotation(DBTableMap.class);
 
 		DBCollection col = MongoDB.getCollection(getDb(tbl), tbl.dbTable());
-		col.ensureIndex(index, indexName);
+		col.createIndex(index, indexName);
 	}
 
 	/**
@@ -239,7 +239,6 @@ public class DataMgr {
 		
 		DBCollection col = MongoDB.getCollection(getDb(tbl), tbl.dbTable());
 		DBObject ret = col.findOne(search, null, sort);
-		col.findOne(search);
 		if (ret == null)
 			return null;
 		
